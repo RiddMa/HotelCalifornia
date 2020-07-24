67b95206388bd015688ac7bc4d137d44da07ad79
Travis换个小黄鸭吧 <7611617+Travislulxy@user.noreply.gitee.com>
2020-07-24 22:14:25 +0800

    usrs包 包括用户类，管理员类，超级管理员类，具体实现未写，只写了方法的结构。

---
+++ usrs/SuperAdmin.java
@@ -0,0 +1,39 @@
+package usrs;
+
+/**
+ * 超级管理员类，继承Admin类
+ * 超级管理员默认为root，密码root
+ */
+public class SuperAdmin extends Admin {
+    /**
+     * 构造函数
+     * 
+     * @param userName 用户名
+     * @param passWord 密码
+     * @param type     用户类型
+     */
+    public SuperAdmin(String userName, String passWord, String type) {
+        super(userName, passWord, type);
+    }
+
+    /**
+     * 超级管理员创建管理员
+     * 
+     * @param userName 用户名
+     * @param passWord 密码
+     * @return Admin对象的引用变量
+     */
+    public Admin createAdmin(String userName, String passWord) {
+        return new Admin(userName, passWord, "admin");
+    }
+
+    /**
+     * 超级管理员删除管理员
+     * 
+     * @param adminUserName 管理员用户名
+     * @return 删除成果返回true，失败（不存在此用户）返回false
+     */
+    public boolean delete(String adminUserName) {
+        return true;
+    }
+}
