51b7e73c120e70684f7334236c6f4451478ff7c0
Travis <Lxy_montage@163.com>
2020-08-12 20:14:04 +0800

    增加了传输层部分的测试类

---
+++ testClient.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import Transport.*;
+
+public class testClient {
+    public static void main(String[] args) {
+        TransportClient b = new TransportClient("localhost", 8088);
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        while(i != 5){
+            i++;
+            String str = sc.nextLine();
+            b.transport(str);
+            //System.out.println("server:" + b.accept());
+        }
+        b.close();
+    }
+}
---
+++ testServer.java
@@ -0,0 +1,17 @@
+import Transport.*;
+import java.util.Scanner;
+
+public class testServer {
+    public static void main(String[] args) {
+        while (true) {
+            TransportServer a = new TransportServer(8088);
+            Scanner sc = new Scanner(System.in);
+            String str;
+            while ((str = a.accept()) != null) {
+                System.out.println("client:" + str);
+            }
+            a.close();
+        }
+
+    }
+}
