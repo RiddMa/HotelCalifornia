4bff92a690a9cb8f86e152119bd5d163f1bf15cd
mochawang <2238857450@qqcom>
2020-08-26 22:01:39 +0800

    Client

--- Client.java
+++ Client.java
@@ -1,17 +1,24 @@
 import Transport.*;
 import command.*;
 
+
 import java.io.*;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class Client {
     private static String command;
     private static String[] args;
-
+    /**
+    *String formatDate = DateUtil.timeStampToFormatDate(Calendar.getInstance().getTime(), "yyyy-MM-dd HH:mm:ss");
+     *         System.out.println("formatDate3: " + String fo);
+     */
     public static void main(String[] arg) {
         TransportClient tc = new TransportClient();
         CommandClient cc = new CommandClient("null", tc);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//接收命令行命令
-        System.out.println("请输入用户名和密码：\n" + "（新用户请先进行注册：create）");
+        String formatDate = Client.timeStampToFormatDate(Calendar.getInstance().getTime(), "yyyy-MM-dd HH:mm:ss");
+        System.out.println(formatDate+"\nlogin"+"请输入用户名和密码：\n" + "（新用户请先进行注册：create）");
         try {
             while (true) {
                 String str = br.readLine();
@@ -21,34 +28,34 @@ public class Client {
                 switch (args[0]) {
                     case "LOGIN":
                         if (cc.parse(str)) {
-                            System.out.println("登录成功");
+                            System.out.println(formatDate+"\nlogin"+"登录成功");
                             switch (cc.getType()) {
                                 case "user":
-                                    System.out.println("你可以进行以下操作：\n" + "注销：logout\n" + "预定房间：reserveRoom\n" + "显示个人预定信息：showReservation");
+                                    System.out.println(formatDate+"\nlogin"+"你可以进行以下操作：\n" + "注销：logout\n" + "预定房间：reserveRoom\n" + "显示个人预定信息：showReservation");
                                     break;
                                 case "admin":
-                                    System.out.println("你可以进行以下操作：\n" + "注销：logout\n" + "预定房间：reserveRoom\n" + "显示个人预定信息：showReservation\n" + "添加旅馆房间信息：addRoom\n" + "查看所有的预定信息：showReservations");
+                                    System.out.println(formatDate+"\nlogin"+"你可以进行以下操作：\n" + "注销：logout\n" + "预定房间：reserveRoom\n" + "显示个人预定信息：showReservation\n" + "添加旅馆房间信息：addRoom\n" + "查看所有的预定信息：showReservations");
                                     break;
                                 case "superadmin":
-                                    System.out.println("你可以进行以下操作：\n" + "注销：logout\n" + "预定房间：reserveRoom\n" + "显示个人预定信息：showReservation\n" + "添加旅馆房间信息：addRoom\n" + "查看所有的预定信息：showReservations\n" + "创建管理员：createAdmin\n" + "删除管理员：delete");
+                                    System.out.println(formatDate+"\nlogin"+"你可以进行以下操作：\n" + "注销：logout\n" + "预定房间：reserveRoom\n" + "显示个人预定信息：showReservation\n" + "添加旅馆房间信息：addRoom\n" + "查看所有的预定信息：showReservations\n" + "创建管理员：createAdmin\n" + "删除管理员：delete");
                                     break;
                             }
                         } else {
-                            System.out.println("登录失败，请重新输入用户名和密码");
+                            System.out.println(formatDate+"\nlogin"+"登录失败，请重新输入用户名和密码");
                         }
                         break;
                     case "LOGOUT":
                         if (cc.parse(str)) {
-                            System.out.println("已登出");
+                            System.out.println(formatDate+"\nlogin"+"已登出");
                         } else {
-                            System.out.println("登出失败");
+                            System.out.println(formatDate+"\nlogin"+"登出失败");
                         }
                         break;
                     case "RESERVEROOM":
                         if (cc.parse(str)) {
-                            System.out.println("预约成功，请按时入住");
+                            System.out.println(formatDate+"\nlogin"+"预约成功，请按时入住");
                         } else {
-                            System.out.println("房间已满，预约失败");
+                            System.out.println(formatDate+"\nlogin"+"房间已满，预约失败");
                         }
                         break;
                     case "SHOWRESERVATIONS":
@@ -59,34 +66,34 @@ public class Client {
                         break;
                     case "CREATEADMIN":
                         if (cc.parse(str)) {
-                            System.out.println("创建成功");
+                            System.out.println(formatDate+"\nlogin"+"创建成功");
                         } else {
-                            System.out.println("创建失败");
+                            System.out.println(formatDate+"\nlogin"+"创建失败");
                         }
                         break;
                     case "ADDROOM":
                         if (cc.parse(str)) {
-                            System.out.println("房间创建成功");
+                            System.out.println(formatDate+"\nlogin"+"房间创建成功");
                         } else {
-                            System.out.println("房间创建失败");
+                            System.out.println(formatDate+"\nlogin"+"房间创建失败");
                         }
                         break;
                     case "DELETE":
                         if (cc.parse(str)) {
-                            System.out.println("删除成功");
+                            System.out.println(formatDate+"\nlogin"+"删除成功");
                         } else {
-                            System.out.println("失败，不存在此用户");
+                            System.out.println(formatDate+"\nlogin"+"失败，不存在此用户");
                         }
                         break;
                     case "CREATE":
                         if (cc.parse(str)) {
-                            System.out.println("注册成功，请登录:login");
+                            System.out.println(formatDate+"\nlogin"+"注册成功，请登录:login");
                         } else {
-                            System.out.println("注册失败");
+                            System.out.println(formatDate+"\nlogin"+"注册失败");
                         }
                         break;
                     default:
-                        System.out.println("unknown command:" + command + ".");
+                        System.out.println(formatDate+"\nlogin"+"unknown command:" + command + ".");
                         break;
                 }
             }
@@ -94,6 +101,10 @@ public class Client {
             e.printStackTrace();
         }
     }
-
+    public static String timeStampToFormatDate(Object dateObj, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        String formatDate = sdf.format(dateObj);
+        return formatDate;
+    }
 
 }
