b2d863ae6f4a578512d64a7d183824355f4481e7
mochawang <2238857450@qqcom>
2020-08-27 15:19:10 +0800

    Client

--- Server.java
+++ Server.java
@@ -1,17 +1,26 @@
 import Transport.TransportServer;
 import command.CommandServer;
 
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
 public class Server {
     public static void main(String[] arg) {
         TransportServer ts = new TransportServer();
         CommandServer cs = new CommandServer("null", ts);
+        String formatDate = Server.timeStampToFormatDate(Calendar.getInstance().getTime(), "yyyy-MM-dd HH:mm:ss");
         String str;
         while (true) {
             while ((str = ts.accept()) != null) {
-                System.out.println("client:" + str);
+                System.out.println(formatDate+"\n"+"client:" + str);
                 cs.parse(str);
             }
             ts.socketAccept();
         }
+    }
+    public static String timeStampToFormatDate(Object dateObj, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        String formatDate = sdf.format(dateObj);
+        return formatDate;
     }
 }
